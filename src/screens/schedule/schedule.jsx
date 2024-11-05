94179ad49b8cf7c9c32d3d4b5a5d899f9921e763
Luciano Silva <luciano_jet@hotmail.com>
2024-11-05 13:33:36 -0300

    ajustado

---
+++ src/screens/schedule/schedule.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+import { styles } from "../../screens/schedule/schedule.style";
+
+
+// fonte do calendario https://github.com/wix/react-native-calendars
+
+function Schedule() {
+    return (
+        <View>
+            <Text>teste</Text>
+        </View>
+    )
+}
+
+export default Schedule;
